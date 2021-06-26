732058d1d3b7054fc90abe525c5a0393a51ba6aa
Aayush Gupta <shraay0528@gmail.com>
2021-06-26 13:30:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 26 12:05:57 2021
+
+@author: guptaa
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+if __name__ == '__main__':
+	app.run()
